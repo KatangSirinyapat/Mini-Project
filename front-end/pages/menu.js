758aa4f417ab621fe6b030c0db4ded96ae5a9bba
sirinyapat <sirinyapat.b@gmail.com>
2021-04-23 01:29:32 +0700

    edit menu page

--- front-end/pages/menu.js
+++ front-end/pages/menu.js
@@ -9,9 +9,10 @@ export default function Menu({ token }) {
       <Head>
         <title>Online Community Blog</title>
       </Head>
+      <div className = 'flex justify-center mt-14 text-4xl font-bold uppercase tracking-wider'>Online Community Blog</div>
+      <div className = 'flex justify-center mt-4 text-md font-medium uppercase tracking-wider italic animate-pulse'>" Please select the faculty you want "</div>
 
-      <div className='md:flex flex-row fixed justify-evenly items-center h-screen w-screen'>
-      
+      <div className='md:flex flex-row fixed justify-evenly items-start mt-36 h-screen w-screen'>  
       <Link href='/fis'>
           <a className='flex flex-col w-56 h-60 bg-sunglow shadow-lg rounded-lg transition duration-700 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
             <div className='-m-10'>
